de602eb4267dc5bcb00eb3fdc7b29c6e3d608ecd
Geoff Crittenden <geoffcritt@hotmail.com>
2019-05-21 05:42:37 -0500

    tweaks to certification services

--- app/models/certification_service.rb
+++ app/models/certification_service.rb
@@ -1,6 +1,7 @@
 class CertificationService < ApplicationRecord
   has_many :certifications
   has_many :certification_labels
+  alias_method :labels, :certification_labels
 
   validates :name, presence: true
 end
